e427f051713ea53a1e64a7bad7a0509fede1dbb1
toneddu2000 <a.latronico@gmail.com>
2014-09-09 15:51:15 +0200

    Player structure functions
    first commit

---
+++ data/code/cs/player/startup.c
@@ -0,0 +1,28 @@
+/*
+  	DPTECH - scratch game framework for Darkplaces Engine http://icculus.org/twilight/darkplaces
+  	Written in 2014 by toneddu2000 (email: a DOT latronico AT gmail DOT com)
+  	Contributors: Spike, ChristianIce
+  	To the extent possible under law, the author(s) have dedicated all copyright and related and neighboring rights to this software to the public domain worldwide. 
+  	This software is distributed without any warranty.
+ 	You should have received a copy of the CC0 Public Domain Dedication along with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+*/
+
+void Player_Startup(float newent)
+{
+	if (newent)
+	{
+		player.classname = "player";
+		setmodelindex(self, player.modelindex); 
+		self.drawmask = MASK_NORMAL; // makes the entity visible
+		
+		//weapon / fire check
+		self.weaponCurrent = 0;
+		self.bCanShoot = 0;
+
+		//skeleton
+		if (!self.skeletonindex){
+			self.skeletonindex = skel_create(self.modelindex);
+			example_skel_player_update_begin(self.modelindex,frameforname(self.modelindex,"idle"),0);
+		}
+	}
+}
